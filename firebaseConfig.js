000f0a78b7a956164a3e95513e7358af4e7c416a
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2024-08-03 13:20:58 -0500

    Configuracion de FireBase

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2qMv6vW61lCYx3bsU6b7u2t5yoXr-pRs",
+    authDomain: "lyco2024.firebaseapp.com",
+    databaseURL: "https://lyco2024-default-rtdb.firebaseio.com",
+    projectId: "lyco2024",
+    storageBucket: "lyco2024.appspot.com",
+    messagingSenderId: "860774540103",
+    appId: "1:860774540103:web:eec921152df4432d816262",
+    measurementId: "G-E5236FD6G7"
+};
+
+export default firebaseConfig;
