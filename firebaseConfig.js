fc16a9c359d6b9d84c5ce631b3b98506daf9bcec
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2024-08-08 22:46:56 -0500

    Eliminando las keys

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,2 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyA2qMv6vW61lCYx3bsU6b7u2t5yoXr-pRs",
-    authDomain: "lyco2024.firebaseapp.com",
-    databaseURL: "https://lyco2024-default-rtdb.firebaseio.com",
-    projectId: "lyco2024",
-    storageBucket: "lyco2024.appspot.com",
-    messagingSenderId: "860774540103",
-    appId: "1:860774540103:web:eec921152df4432d816262",
-    measurementId: "G-E5236FD6G7"
-};
-
-export default firebaseConfig;
+//xd no debí subir esta ~wbd~
+//Las configuraciones previas han sido eliminadas
